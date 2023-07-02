3905cd4442fc7466c2e46814de8b2c97edfab001
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-02 12:08:31 +0800

    completed part 2 - exercise 2.13

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import personService from './services/persons';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
@@ -11,10 +11,10 @@ const App = () => {
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then((response) => {
-        setPersons(response.data);
+    personService
+      .getAll()
+      .then((initialPersons) => {
+        setPersons(initialPersons);
       });
   }, [])
 
@@ -41,12 +41,11 @@ const App = () => {
       return alert(`${newPerson.name} is already added to phonebook`);
     }
 
-    axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then((response) => response.data)
+    personService
+      .create(newPerson)
       .then((returnedPerson) => {
         setPersons([...persons, returnedPerson]);
-      })
+      });
   }
 
   const personsToShow = filter 
