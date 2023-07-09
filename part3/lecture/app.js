ad3950cdee53bb9ba4935a8a117b2bc7931082c5
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-09 08:15:44 +0800

    updated project structure of part 3

---
+++ part3/lecture/app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/notes', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- part3/lecture/models/note.js
+++ part3/lecture/models/note.js
@@ -1,16 +1,4 @@
 const mongoose = require('mongoose');
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-
-mongoose.set('strictQuery', false);
-mongoose.connect(url)
-  .then((result) => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
 
 const noteSchema = new mongoose.Schema({
   content: {
