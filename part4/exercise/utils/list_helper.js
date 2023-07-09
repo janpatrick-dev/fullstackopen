14c452f88a940cf87f889307d6fe45575fe944d4
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-09 12:53:08 +0800

    completed part 4 - exercise 4.5

--- part4/exercise/utils/list_helper.js
+++ part4/exercise/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((favorite, blog) => {
+    if (!favorite || blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+    return favorite;
+  }, null);
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
