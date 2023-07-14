f7b59f79a659eff7129cec94941e22c0dc882e33
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-14 21:49:19 +0800

    completed part 4 - exercise 4.21

--- part4/exercise/controllers/blogs.js
+++ part4/exercise/controllers/blogs.js
@@ -51,4 +51,33 @@ blogsRouter.post('/', async (request, response) => {
   }
 });
 
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'invalid token' });
+    }
+
+    const blog = await Blog.findById(request.params.id);
+
+    if (!blog) {
+      return response.status(401).json({ error: 'blog does not exist!' });
+    }
+
+    if (decodedToken.id !== blog.user.toString()) {
+      return response.status(401).json({ error: 'unauthorized access' });
+    }
+
+    const user = await User.findById(blog.user.toString());
+    user.blogs = user.blogs.filter((b) => b.toString() !== blog.toString());
+
+    await user.save();
+    await blog.deleteOne();
+    
+    response.status(204).json({});
+  } catch (exception) {
+    response.status(500).json({ error: exception.message });
+  }
+});
+
 module.exports = blogsRouter;
