623ebd5d453265c6e9138a9c43423d5b5f25b7b6
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-19 23:24:22 +0800

    completed part 6 - exercise 6.12

--- part6/exercise/anecdotes/src/App.js
+++ part6/exercise/anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import Filter from './components/Filter';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
+import Notification from './components/Notification';
 
 const App = () => {
   return (
