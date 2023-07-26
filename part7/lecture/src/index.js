f1e475154dc9ceb4bce10fbabb60743210ba6a7c
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-26 13:41:09 +0800

    updated part 7 lecture code

--- part7/lecture/src/index.js
+++ part7/lecture/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Container>
+    <Router>
+      <App />
+    </Router>
+  </Container>
 );
