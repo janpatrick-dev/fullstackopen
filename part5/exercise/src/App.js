6cafb588677d1445106eccd0abd0e8924e890f58
janpatrickdev <janpatrick.dev@gmail.com>
2023-08-10 19:20:42 +0800

    completed part 7 - exercise 7.17

--- part5/exercise/src/App.js
+++ part5/exercise/src/App.js
@@ -3,21 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import Blogs from './components/Blogs';
 import Login from './components/Login';
 import { useDispatch, useSelector } from 'react-redux';
-import { initializeUser, logoutUser } from './reducers/userReducer';
+import { initializeUser } from './reducers/userReducer';
 import Notification from './components/Notification';
 import Users from './components/Users';
 import User from './components/User';
 import Blog from './components/Blog';
+import Navigation from './components/Navigation';
 
 const App = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.users.user);
   const success = useSelector(state => state.notification.success);
   const error = useSelector(state => state.notification.error);
-
-  const handleLogout = () => {
-    dispatch(logoutUser());
-  };
 
   useEffect(() => {
     dispatch(initializeUser());
@@ -29,14 +26,11 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
+      <Navigation />
+      <h2>blog app</h2>
       <Notification
         className={error ? 'error' : 'success'}
         message={error || success} />
-      <p>
-        {user.name} logged in
-        <button onClick={handleLogout}>logout</button>
-      </p>
       <Routes>
         <Route path='/' element={<Blogs />} />
         <Route path='/blogs/:id' element={<Blog />} />
--- part5/exercise/src/components/BlogRow.js
+++ part5/exercise/src/components/BlogRow.js
@@ -11,43 +11,10 @@ const BlogRow = ({ blog }) => {
     marginBottom: 5
   };
 
-  // const handleLike = async (e, blog) => {
-  //   e.preventDefault();
-
-  //   await dispatch(incrementBlogLikes(blog));
-  // };
-
-  // const handleDelete = async (e, blogToDelete) => {
-  //   e.preventDefault();
-
-  //   await dispatch(deleteBlog(blogToDelete));
-  // };
-
-  // const removeButton = () => {
-  //   if (blog.user.username === user.username) {
-  //     return (
-  //       <button onClick={(e) => handleDelete(e, blog)} data-testid='removeButton'>
-  //         remove
-  //       </button>
-  //     );
-  //   }
-  // };
-
   return (
     <div style={blogStyle} className='blog'>
       <Link to={`/blogs/${blog.id}`}>
         {blog.title} {blog.author}
-        {/* <Togglable buttonLabel='view' type='blog' className='blogTogglable'>
-          <div>{ blog.url }</div>
-          <div>
-            <span data-testid='blogLikes'>
-              likes { blog.likes }
-              <button onClick={(e) => handleLike(e, blog)}>like</button>
-            </span>
-          </div>
-          <div>{ blog.user && blog.user.name }</div>
-          { removeButton() }
-        </Togglable> */}
       </Link>
     </div>
   );
