f6a3df5f20b6d8a00c0f55e6f99b654c63760c64
janpatrickdev <janpatrick.dev@gmail.com>
2023-08-18 13:21:12 +0800

    completed part 8 - exercise 8.8 to 8.10

---
+++ part8/exercise/library-frontend/src/components/Books.js
@@ -0,0 +1,37 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from "../queries"
+
+const Books = (props) => {
+  const query = useQuery(ALL_BOOKS);
+
+  if (!props.show || query.loading) {
+    return null
+  }
+
+  const books = query.data.allBooks;
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
