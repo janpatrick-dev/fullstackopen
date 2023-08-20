e0a0caf86612fb57bd56a321541e40bd651db065
janpatrickdev <janpatrick.dev@gmail.com>
2023-08-20 20:38:36 +0800

    completed part 8 - exercise 8.25

--- part8/exercise/library-frontend/src/App.js
+++ part8/exercise/library-frontend/src/App.js
@@ -6,13 +6,42 @@ import LoginForm from './components/LoginForm'
 import Recommendations from './components/Recommendations'
 import { Routes, Route } from 'react-router-dom';
 import UserActions from './components/UserActions'
-import { useQuery, useSubscription } from '@apollo/client'
-import { BOOK_ADDED, USER } from './queries'
+import { useApolloClient, useQuery, useSubscription } from '@apollo/client'
+import { ALL_BOOKS, BOOK_ADDED, USER } from './queries'
+
+export const updateBookCache = (cache, addedBook) => {
+  const uniqByName = (a) => {
+    let seen = new Set();
+    return a.filter((item) => {
+      let k = item.title;
+      return seen.has(k) ? false : seen.add(k);
+    });
+  };
+
+  cache.updateQuery({ query: ALL_BOOKS, variables: { genre: '' }}, (data) => {
+    if (!data) { return null };
+    return {
+      allBooks: uniqByName(data.allBooks.concat(addedBook))
+    };
+  });
+  
+  if (addedBook.genres) {
+    addedBook.genres.forEach((genre) => {
+      cache.updateQuery({ query: ALL_BOOKS, variables: { genre }}, (data) => {
+        if (!data) { return null };
+        return {
+          allBooks: uniqByName(data.allBooks.concat(addedBook))
+        };
+      });
+    });
+  } 
+}
 
 const App = () => {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
   const userQuery = useQuery(USER);
+  const client = useApolloClient();
 
   useEffect(() => {
     if (userQuery.data && userQuery.data.me) {
@@ -29,7 +58,9 @@ const App = () => {
 
   useSubscription(BOOK_ADDED, {
     onData: ({ data }) => {
-      alert(data);
+      const addedBook = data.data.bookAdded;
+      
+      updateBookCache(client.cache, addedBook);
     }
   });
 
