12649ea6f1cc1f10037b28e5f4df18fa24843001
janpatrickdev <janpatrick.dev@gmail.com>
2023-08-20 19:15:56 +0800

    updated part 8 - lecture code fragments and subscriptions

--- part8/lecture/client/src/App.js
+++ part8/lecture/client/src/App.js
@@ -1,10 +1,26 @@
-import { useApolloClient, useQuery } from '@apollo/client';
+import { useApolloClient, useQuery, useSubscription } from '@apollo/client';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
-import { ALL_PERSONS } from './queries';
+import { ALL_PERSONS, PERSON_ADDED } from './queries';
 import { useState } from 'react';
 import PhoneForm from './components/PhoneForm';
 import LoginForm from './components/LoginForm';
+
+export const updateCache = (cache, query, addedPerson) => {
+  const uniqByName = (a) => {
+    let seen = new Set();
+    return a.filter((item) => {
+      let k = item.name;
+      return seen.has(k) ? false : seen.add(k);
+    })
+  };
+
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson))
+    }
+  });
+}
 
 const App = () => {
   const [token, setToken] = useState(null);
@@ -24,6 +40,14 @@ const App = () => {
     localStorage.clear();
     client.resetStore();
   }
+
+  useSubscription(PERSON_ADDED, {
+    onData: ({ data }) => {
+      const addedPerson = data.data.personAdded;
+      notify(`${addedPerson.name} added`);
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson);
+    }
+  })
 
   if (!token) {
     return (
